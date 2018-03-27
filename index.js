cd09db49557a2c15145e42e8057cbafd6b83511c
Ray Snider <ray.snider21@gmail.com>
2018-03-27 13:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+const app = "I do much."
+
+function destructivelyAppendKitten(name)
+{
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name)
+{
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten()
+{
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  return kittens.shift();
+}
+
+function appendKitten(name)
+{
+  var newKitten = [kittens,...name];
+  return newKitten;
+}
